839c4068dc7bf014f5148599cbc42f3fd18598cf
Janice Hixon <102612342+jhixon2@users.noreply.github.com>
2024-09-30 15:23:47 -0500

    Added a new VR scene for meta headsets

--- Codes/VR/TMS VR SteamVR [Testing]/Assets/Scripts/Shooting/Hittable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IHittable 
-{
-    void OnHit(Vector3 position);
-    Vector3 GetPosition();
-}
--- Codes/VR/TMS VR SteamVR [Testing]/Assets/Scripts/Unity Editor Modifications/ReadOnlyAttribute.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-public class ReadOnlyAttribute : PropertyAttribute
-{
-    /* This class' declaration is necessary for the ReadOnlyDrawer class in Assets/Editor/ReadOnlyDrawer.cs */
-}
