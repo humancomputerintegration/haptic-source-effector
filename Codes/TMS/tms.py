f541243358dbd028162a3585ad52ed618b9e1c00
Human Computer Integration Lab <humancomputerintegration+github@gmail.com>
2024-11-03 19:33:06 -0600

    Update tms.py

--- Codes/TMS/tms.py
+++ Codes/TMS/tms.py
@@ -8,22 +8,25 @@ from typing import List
 
 ip = "127.0.0.1" #OSC IP address; here, just for local communication
 
-# TMS OSC port
+# MMS OSC port
 serverPort = 6969
 clientPort = 6161
 
-#TMS unlock code
-unlockCode = '7cef-ae8082c7-0e'
-
 def armStim(address, *args):
+    # if not myMagstim.isArmed():
     myMagstim.arm(receipt=False, delay=True)
     print("Magstim is armed")
+    # else:
+    #     print(myMagstim.isArmed())
     armed = True
     client.send_message("/armed", 1)
 
 def disarmStim(address, *args):
+    #if myMagstim.isArmed():
     myMagstim.disarm(receipt=False)
     print("Magstim is disarmed")
+    # else:
+    #     print("Magstim is already disarmed")
     armed = False
     client.send_message("/armed", 0)
 
@@ -40,14 +43,18 @@ def stimQuick(address, *args: List[any]): #output stim with minimal latency; thi
     duration = args[0]
     myMagstim.ignoreCoilSafetySwitch()
     myMagstim.fire(receipt=False)
+    #sleep(duration)
+    #myMagstim.disarm(receipt=False)
 
 def stimUntilStop(address, *args: List[any]): #output stim until it stopped externally; this induces latency before and after
     trigger = args[0]
     if trigger == 1:
         myMagstim.ignoreCoilSafetySwitch()
         myMagstim.fire(receipt=False)
+        #myMagstim.ignoreCoilSafetySwitch()
     else:
         myMagstim.disarm(receipt=False)
+        #myMagstim.arm(receipt=False, delay=False)
 
 def getInfo(address, *args):
     print(myMagstim.getParameters())
@@ -55,6 +62,7 @@ def getInfo(address, *args):
 def quit(address, *args):
     myMagstim.disconnect()
     sys.exit()
+    #client.send_message("/connection", 0)
 
 def config(intensity, frequency, period): #this must be executed before any stim functions; this takes roughly 250 ms to execute
     myMagstim.setPower(intensity, receipt=False, delay=False)
@@ -62,14 +70,19 @@ def config(intensity, frequency, period): #this must be executed before any stim
     myMagstim.setDuration(period, receipt=False)
     myMagstim.validateSequence()
 
-if __name__ == "__main__": 
-    # use the following for mac/linux 
-    # myMagstim = magstim.Rapid(serialConnection='/dev/ttyS8', superRapid=1, unlockCode=unlockCode) #magstim initialization
-    # use the following for Windows 
-    myMagstim = magstim.Rapid(serialConnection='COM8', superRapid=1, unlockCode=unlockCode) #magstim initialization
+if __name__ == "__main__": #the library owner says this is necessary to avoid collapse in Windows
+    # use the following for mac/linux (currently Aki's pc)
+    # myMagstim = magstim.Rapid(serialConnection='/dev/ttyS8', superRapid=1, unlockCode='7cef-ae8082c7-0e') #magstim initialization
+    # use the following for Windows (currently lab's pc)
+    myMagstim = magstim.Rapid(serialConnection='/dev/tty.usbserial-14410', superRapid=1, unlockCode='7cef-ae8082c7-0e') #magstim initialization
 
     myMagstim.connect() #connect to magstim device via serial
     myMagstim.rTMSMode(enable=True) #activate rTMS
+
+    # myMagstim.arm(receipt=False, delay=True)
+    # armed = True
+
+    # r = Rehamove("COM14")
 
     dispatcher = dispatcher.Dispatcher()
     dispatcher.map("/arm", armStim)
